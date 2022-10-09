01652155e6b52361611d2c6f914fa0313bb6e62d
Saurav <spike.samantray@gmail.com>
2022-10-09 10:00:18 +0530

    removed logs and target from git

--- target/classes/static/js/survey.js
+++
@@ -1,7 +0,0 @@
-var questions = [[${questions}]];
-    $('form').on('submit', function(event) {
-        event.preventDefault();
-        //$('#hiddenInput').val(someVariable); //perform some operations
-        alert(questions);
-        //this.submit(); //now submit the form
-    });
